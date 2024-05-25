d0d5c0933aaced900ab3cbc3d855f49aa13fc636
Samuel <smltrs0@gmail.com>
2024-05-25 14:09:31 -0500

    feat: Update UserService to use Prisma for database operations

--- data/Model/UserService.ts
+++ data/Model/UserService.ts
@@ -1,14 +1,20 @@
+import { PrismaClient } from "@prisma/client";
 
 export default  class UserService {
+    private prisma: PrismaClient;
     constructor() {
+        this.prisma = new PrismaClient();
     }
     async getUserByEmail(email: string) {
         return { email: email };
     }
     async createUser(user: any) {
-        return user;
+        return this.prisma.user.create({ data: user });
     }
-    async getUserById(id: string) {
-        return { id: id };
+    async getUserById(id: number) {
+        return this.prisma.user.findUnique({ where: { id: id } });
+    }
+    async getUsers() {
+        return this.prisma.user.findMany();
     }
 }

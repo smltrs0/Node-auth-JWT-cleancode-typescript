f400c7d2f854fbaeb828546fd298244e49897d42
Samuel <smltrs0@gmail.com>
2024-05-25 15:31:15 -0500

    feat: Fix missing id validation in UserMapper

--- src/infrastructure/mappers/user.mapper.ts
+++ src/infrastructure/mappers/user.mapper.ts
@@ -6,7 +6,7 @@ export class UserMapper{
     static userEntiFromObject(object: { [key : string] : any}) {
         
         const { _id, id, username, email, password } = object;
-        if (!id && !_id) throw CustomError.badRequest('Missing id');
+
         if (!username) throw CustomError.badRequest('Missing name');
         if (!email) throw CustomError.badRequest('Missing email');
         
